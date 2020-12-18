25fc7620685e1fa6b00ea2ba74e9d2e7ddc5d545
rifki bogara <b0g4r4@gmail.com>
2020-12-18 13:16:10 +0000

    rapikan tampilan

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var nik = prompt ('Masukkan 16 digit nik: ')
 var bod = prompt ('Masukkan 8 digit tanggal lahir seperti contoh sbb yyyy-mm-dd: ')
 var person = new PersonAgeLogic(name, nik, bod);
 console.log(person.getWeekType())
-console.log(`Saat ini berumur${person.getAge()}`)
+console.log(`Saat ini berumur ${person.getAge()}`)
 console.log(person.personByAge())
 }
 
