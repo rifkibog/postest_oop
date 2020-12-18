734a783862790a675a410f9ce7f33d066224a656
b0g4r4 <b0g4r4@gmail.com>
2020-12-18 21:52:46 +0700

    initial program

--- Domain/PersonCheck/Person.Check.js
+++ Domain/PersonCheck/Person.Check.js
@@ -1,6 +1,7 @@
-var PersonLogic= require('../Person/Person.Logic')
+//var PersonLogic= require('../Person/Person.Logic')
+var PersonAgeLogic= require('../PersonAge/Person.Age')
 
-class PersonCheckLogic extends PersonLogic{
+class PersonCheckLogic extends PersonAgeLogic{
     constructor(...args) {
     super(...args)
   }
@@ -10,24 +11,24 @@ class PersonCheckLogic extends PersonLogic{
     if (`19${this.nik.slice(10,12)}`>1970 && `19${this.nik.slice(10,12)}`<2000){
         var nikcek = `19${this.nik.slice(10,12)}-${this.nik.slice(8,10)}-${this.nik.slice(6,8)}`
         if (nikcek === this.bod){
-            return "data nik valid with bod, anda kelahiran tahun 19xx"
+            return "data nik valid dengan tanggal lahir, anda kelahiran tahun 19xx"
         } //data ok tuk tahun kelahiran dibawah 2000
         else {
-            return "data nik not valid with bod"
+            return "data nik tidak valid dengan tanggal lahir"
         } // data nok tuk tahun kelahiran dibawah 2000
     }
     else{
         var nikcek = `20${this.nik.slice(10,12)}-${this.nik.slice(8,10)}-${this.nik.slice(6,8)}`
         if (nikcek === this.bod){
-            return "data nik valid with bod, anda kelahiran tahun 20xx"
+            return "data nik valid dengan tanggal lahir, anda kelahiran tahun 20xx"
         }
         else {
-            return "data nik not valid with bod"
+            return "data nik tidak valid dengan tanggal lahir"
         } // data nok tuk tahun kelahiran diatas 2000
     } // cek tahun
   }//cek number digit nik
   else{
-      return "data nik nok"
+      return "data nik tidak tepat/kurang"
   }
        
      
