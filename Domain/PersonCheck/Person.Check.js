4631e24814022e7fa1f60fe37181d2be10d38bee
b0g4r4 <b0g4r4@gmail.com>
2020-12-18 21:02:37 +0700

    initial program with chek nik id

--- Domain/PersonCheck/Person.Check.js
+++ Domain/PersonCheck/Person.Check.js
@@ -6,28 +6,28 @@ class PersonCheckLogic extends PersonLogic{
   }
 
   CheckNIK() {
-  if (this.nik.length = 16){
-    if (this.nik.slice(0,4)>1970 && this.nik.slice(0,4)<2000){
-        var nikcek1 = `19${this.nik.slice(10,12)}-${this.nik.slice(8,10)}-${this.nik.slice(6,8)}`
-        if (nikcek1 == this.nik.slice(0,4)){
-            return "data valid"
+  if (this.nik.length == 16){
+    if (`19${this.nik.slice(10,12)}`>1970 && `19${this.nik.slice(10,12)}`<2000){
+        var nikcek = `19${this.nik.slice(10,12)}-${this.nik.slice(8,10)}-${this.nik.slice(6,8)}`
+        if (nikcek === this.bod){
+            return "data nik valid with bod, anda kelahiran tahun 19xx"
         } //data ok tuk tahun kelahiran dibawah 2000
         else {
-            return "data not valid"
+            return "data nik not valid with bod"
         } // data nok tuk tahun kelahiran dibawah 2000
     }
     else{
-        var nikcek2 = `20${this.nik.slice(10,12)}-${this.nik.slice(8,10)}-${this.nik.slice(6,8)}`
-        if (nikcek2 == this.nik.slice(0,4)){
-            return "data valid"
+        var nikcek = `20${this.nik.slice(10,12)}-${this.nik.slice(8,10)}-${this.nik.slice(6,8)}`
+        if (nikcek === this.bod){
+            return "data nik valid with bod, anda kelahiran tahun 20xx"
         }
         else {
-            return "data not valid"
+            return "data nik not valid with bod"
         } // data nok tuk tahun kelahiran diatas 2000
     } // cek tahun
   }//cek number digit nik
   else{
-      return "data nok"
+      return "data nik nok"
   }
        
      
